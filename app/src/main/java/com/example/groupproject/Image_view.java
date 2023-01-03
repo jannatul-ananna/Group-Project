6784358cfffb4a260b595e12e1998cce1034fba6
Ashik E Elahi <ashikeelahi92@gmail.com>
2023-01-04 01:57:19 +0600

    pushhhhhhhhhhhhhhhhhh

--- app/src/main/java/com/example/groupproject/Image_view.java
+++ app/src/main/java/com/example/groupproject/Image_view.java
@@ -2,13 +2,29 @@ package com.example.groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Image_view extends AppCompatActivity {
 
+    Button buttonBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_view);
+
+        //going back to activity_main
+        buttonBack = findViewById(R.id.button_image_back);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //calling MainActivity.java
+                Intent callMain = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(callMain);
+            }
+        });
     }
 }
