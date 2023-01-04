ac17dbab5445c399deae0c6401fd1d3408b05d88
masfiqq <mm.masfiq5779@gmail.com>
2023-01-04 21:20:09 +0600

    Animation xml

--- app/src/main/java/com/example/groupproject/AnimActivity.java
+++ app/src/main/java/com/example/groupproject/AnimActivity.java
@@ -1,4 +1,14 @@
 package com.example.groupproject;
 
-public class AnimActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnimActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anim);
+    }
 }
