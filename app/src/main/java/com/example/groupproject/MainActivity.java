ef568f00e4faa75e004bb6214fd9a5558c0953f0
Ashik E Elahi <ashikeelahi92@gmail.com>
2023-01-04 20:14:35 +0600

    ulala

--- app/src/main/java/com/example/groupproject/MainActivity.java
+++ app/src/main/java/com/example/groupproject/MainActivity.java
@@ -11,15 +11,15 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnimage;
+    Button btnImage, btnToast, btnGrid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Button btn = findViewById(R.id.btnok);
-        btn.setOnClickListener(new View.OnClickListener() {
+        //Action Toast
+        btnToast = findViewById(R.id.Btn_Toast);
+        btnToast.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "This Listener Button is Clicked", Toast.LENGTH_SHORT).show();
@@ -27,20 +27,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Action Grid
+        btnGrid =findViewById(R.id.btn_grid);
+        btnGrid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGrindView();
+            }
+        });
 
-
-        btnimage = findViewById(R.id.btn2);
-        btnimage.setOnClickListener(new View.OnClickListener() {
+        //Action Image
+        btnImage = findViewById(R.id.btn_Image);
+        btnImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Go To New page for viewing image", Toast.LENGTH_SHORT).show();
-                openGridView();
+                openImageView();
             }
         });
     }
 
-    public void openGridView(){
-        Intent intent = new Intent(getApplicationContext(),Image_view.class);
-        startActivity(intent);
+    public void openImageView(){
+        Intent openImage = new Intent(getApplicationContext(),Image_view.class);
+        startActivity(openImage);
+    }
+
+    public void openGrindView(){
+        Intent openGrid = new Intent(getApplicationContext(),GridViewActivity.class);
+        startActivity(openGrid);
     }
 }
---
+++ app/src/main/java/com/example/groupproject/TableViewActivity.java
@@ -0,0 +1,19 @@
+package com.example.groupproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class TableViewActivity extends Activity {
+
+    Button btnTabNext, btnTabBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_table);
+
+
+    }
+
+
+}
